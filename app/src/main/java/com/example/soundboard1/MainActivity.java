c87da71e8a5883a6243f6e1c0522ce2c3b0c258e
Alexander Herrera <200913831@INV515567.lbusd.local>
2023-06-01 11:29:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/soundboard1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.soundboard1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    Button btnBeastRoar;
+
+    Button btnArrowWoosh;
+
+    Button btnArrowWhoosh;
+
+    Button btnTunnelReverb;
+
+    Button btnWhooshtransition;
+
+    Button btnClassicAlarm;
+
+    Button btnCrowdLaugh;
+
+    Button btnDogBarking;
+
+    Button btnRocketWhoosh;
+
+    Button btnSmallSweep;
+
+    Button btnAsonishedGasp;
+
+    Button btnAraceRetro;
+
+    Button btnHeartBeat;
+
+
+    //Button
+    btnBeastRoar = findViewById(R.id.btn_BeastRoar);
+
+
+    }
+}
